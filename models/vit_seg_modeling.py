afd1d2709edaf48f06d3047b8e4203f2d251df07
YEJIIIIIN <149644848+YEJIIIIIN@users.noreply.github.com>
2024-03-21 17:43:36 +0900

    Add files via upload

---
+++ models/vit_seg_modeling.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import logging
+import math
+
+from os.path import join as pjoin
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNorm
+from torch.nn.modules.utils import _pair
+from scipy import ndimage
+from . import vit_seg_configs as configs
+from .vit_seg_modeling_resnet_skip import ResNetV2
+
+
+CONFIGS = {
+    'R50-ViT-B_16': configs.get_r50_b16_config(),
+    'testing': configs.get_testing(),
+}
+
+
